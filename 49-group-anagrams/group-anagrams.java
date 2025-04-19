ffd8d3054de473e7aca7acd8b88c736aafdbcd31
Madasamy <145176509+madasamydfan@users.noreply.github.com>
2025-04-19 13:12:53 +0530

    Time: 6 ms (98.04%) | Memory: 48 MB (56.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,19 +1,38 @@
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         HashMap<String,List<String>> map = new HashMap<>();
+//         for(String word : strs){
+//             char[] sortedWord = word.toCharArray();
+//             Arrays.sort(sortedWord);
+//             String sortedWordString = new String(sortedWord);
+//             if(!map.containsKey(sortedWordString)){
+//                 map.put(sortedWordString,new ArrayList<>());
+//             }
+//             map.get(sortedWordString).add(word);
+//         }
+//         List<List<String>> list = new ArrayList<>();
+//         for(String w:map.keySet()){
+//             list.add(map.get(w));
+//         }
+//         return list;
+//     }
+// }
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<>();
-        for(String word : strs){
-            char[] sortedWord = word.toCharArray();
-            Arrays.sort(sortedWord);
-            String sortedWordString = new String(sortedWord);
-            if(!map.containsKey(sortedWordString)){
-                map.put(sortedWordString,new ArrayList<>());
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            map.get(sortedWordString).add(word);
+            
+            map.get(sortedWord).add(word);
         }
-        List<List<String>> list = new ArrayList<>();
-        for(String w:map.keySet()){
-            list.add(map.get(w));
-        }
-        return list;
+        
+        return new ArrayList<>(map.values());
     }
 }
